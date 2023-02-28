330273b895fe2ab48eff716cc7653cffa48ad274
Ezequiel Novoa <ezequiel.novoa3@gmail.com>
2023-02-27 16:16:52 -0800

    Create block class contains expected data fields.

---
+++ Project/block.js
@@ -0,0 +1,30 @@
+const { SHA256 } = require("crypto-js");
+
+class Block {
+  constructor(data) {
+    this.id = 0;
+    this.nonce = 144444;
+    this.body = data;
+    this.hash = "";
+  }
+
+  /**
+   * Step 1. Implement `generateHash()`
+   * method that return the `self` block with the hash.
+   *
+   * Create a Promise that resolve with `self` after you create
+   * the hash of the object and assigned to the hash property `self.hash = ...`
+   */
+  //
+  generateHash() {
+    // Use this to create a temporary reference of the class object
+    let self = this;
+    //Implement your code here
+    return new Promise(function (resolve, reject) {
+      self.hash = SHA256(JSON.stringify(self).toString());
+      resolve(self);
+    });
+  }
+}
+
+module.exports.Block = Block;
